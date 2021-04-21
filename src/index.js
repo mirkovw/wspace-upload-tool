1df4e4989239f9dc3b3b6c2abc8c32c538370e22
Mirko <mirko.van.willigenburg@gmail.com>
2021-04-21 17:49:31 +0200

    Save cookies to file for future use

--- src/index.js
+++ src/index.js
@@ -28,6 +28,14 @@ const { JSDOM } = jsdom;
     axiosCookieJarSupport(axios);
     const cookieJar = new tough.CookieJar();
 
+    console.log('Checking if cookies.json exists...')
+    if (await fs.pathExists('./cookies.json')) {
+        console.log('Found. Importing cookies')
+        let cookieData = await fs.readFile('cookies.json');
+        cookieJar._importCookies(JSON.parse(cookieData)); //deserialize function is not available. not going to adjust source...
+    } else {
+        console.log('Not found. Logging in with data from settings.json')
+    }
     const uploadUrl = "https://workspace.mediamonks.com/backend/project-folder/materials-upload?id=" + libraryId;
 
     const config = {
@@ -43,44 +51,57 @@ const { JSDOM } = jsdom;
         jar: cookieJar
     };
 
+    if (cookieJar.toJSON().cookies.length < 2) {
 
-    //get userId and id
-    try {
-        const res = await axios.post(
-            'https://sso-api.theasset.store/api/Accounts/login',
-            {
-                "email": settings.login,
-                "password": settings.password
-            },
-            config
-        );
+        //get userId and id
+        try {
+            console.log("POST: https://sso-api.theasset.store/api/Accounts/login");
+            const res = await axios.post(
+                'https://sso-api.theasset.store/api/Accounts/login',
+                {
+                    "email": settings.login,
+                    "password": settings.password
+                },
+                config
+            );
 
-        userId = res.data.userId;
-        config.headers = {
-            ...config.headers,
-            'Authorization': res.data.id
-        }
-    } catch (err) { }
+            userId = res.data.userId;
+            config.headers = {
+                ...config.headers,
+                'Authorization': res.data.id
+            }
+        } catch (err) { }
 
-    // get get loginUrl
-    try {
-        const res = await axios.get('https://sso-api.theasset.store/api/Accounts/issueTempToken?redirectUrl=https%3A%2F%2Fworkspace.mediamonks.com', config);
-        finalLoginUrl = res.data.loginUrl;
-    } catch (err) { }
+        // get get loginUrl
+        try {
+            console.log("GET: https://sso-api.theasset.store/api/Accounts/issueTempToken?redirectUrl=https%3A%2F%2Fworkspace.mediamonks.com");
+            const res = await axios.get('https://sso-api.theasset.store/api/Accounts/issueTempToken?redirectUrl=https%3A%2F%2Fworkspace.mediamonks.com', config);
+            finalLoginUrl = res.data.loginUrl;
+        } catch (err) { }
 
-    // get _csrf cookie set
-    try {
-        await axios.get(finalLoginUrl, config);
-    } catch (err) { }
+        // get _csrf cookie set
+        try {
+            console.log("GET: " + finalLoginUrl);
+            await axios.get(finalLoginUrl, config);
+        } catch (err) { }
 
+    }
 
     // get csrf token for upload, fish it from the dom
     try {
+        console.log("GET: " + uploadUrl);
         const res = await axios.get(uploadUrl, config);
         dom = new JSDOM(res.data);
         csrfToken = dom.window.document.querySelector('[name="_csrf"]').getAttribute("value");
+        console.log('Succes. CSRF token = ' + csrfToken)
     }
     catch (err) { }
+
+
+    console.log('Writing cookies to file..');
+    const cookies = cookieJar.toJSON();
+    let cookie_data = JSON.stringify(cookies, null, 2);
+    await fs.writeFile('cookies.json', cookie_data);
 
 
     // upload file
@@ -94,8 +115,9 @@ const { JSDOM } = jsdom;
     }
 
     try {
+        console.log("POST: " + uploadUrl);
         const res = await axios.post(uploadUrl, data, config);
-        console.log('Succes!')
+        console.log('Upload succesful.')
     }
 
     catch (err) {
